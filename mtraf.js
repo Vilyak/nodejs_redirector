61b9ef7526496cc98e24cede7fbcf78be5b8f5af
dzmitryv <dzmitryv@playtika.com>
2023-03-14 17:09:16 +0300

    Add mtraf rules

--- mtraf.js
+++ mtraf.js
@@ -7,22 +7,27 @@ const OFFER = 'https://ttdomen.top/click?o=2&a=1252&aff_param1=tiktok';
 
 const WHITE_PAGE = 'https://www.facebook.com/';
 
+let counter = 0;
+
 app.use('*', function (request, response, next) {
     try {
         const userAgent = request.get('user-agent');
         const platform = request.useragent.platform;
 
-        if (userAgent.toString().toLowerCase().includes('bot')
-            || platform.toLowerCase().includes('windows')
-            || platform.toLowerCase().includes('macintosh')
-            || platform.toLowerCase().includes('linux')
-        ) {
+        const isBotDefinition = !!userAgent.toString().toLowerCase().includes('bot');
+
+        if (!isBotDefinition && (
+            platform.toLowerCase().includes('iphone') ||
+            platform.toLowerCase().includes('android') ||
+            platform.toLowerCase().includes('ipad')
+        )) {
+            counter++;
+            console.log(`[${counter}][${platform}] OFFER`);
+            response.redirect(OFFER);
+        } else {
+            console.log('[BOT] WHITE PAGE');
             response.redirect(WHITE_PAGE);
         }
-        else {
-            response.redirect(OFFER);
-        }
-
     } catch (e) {
         console.log(e);
         response.redirect(WHITE_PAGE);
