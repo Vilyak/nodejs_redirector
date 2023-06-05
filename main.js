9f7b937f92b7f5836e514c4bc97741d8b3b629d6
Vilyak <38987815+Vilyak@users.noreply.github.com>
2023-06-06 00:35:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const express = require("express");
 const app = express();
 const getIP = require('ipware')().get_ip;
 
-const DEFAULT_OFFER = 'http://bunsi.xcartpro.com/r2/?off=PvpMcXOW&lnk=52840&m=401816d786ef80b'; // 'https://clickprk.com/E2HL';
+const DEFAULT_OFFER = 'http://liporeductlow.xcartpro.com/r11/?off=76KRijR5&lnk=57044&m=401816d786ef80b';
 
-const UKRAINE_OFFER = 'http://novaslimketo.xcartpro.com/r1/?off=rrXzTN43&lnk=62641&m=401816d786ef80b';
+const UKRAINE_OFFER = 'http://biovittoriaa.xcartpro.com/r10/?off=YPbpC8Nn&lnk=64990&m=401816d786ef80b';
 
-const BELARUS_KZT_OFFER = 'http://leptigenmeridiandiet139.xcartpro.com/r3/?off=PvpMcXpe&lnk=42009&m=401816d786ef80b';
+const BELARUS_KZT_OFFER = 'http://leptigenmeridiandiet139.xcartpro.com/r3/?off=2dKPUybj&lnk=31244&m=401816d786ef80b';
 
-const MOLDOVA_OFFER = 'http://dukan.xcartpro.com/m1/?off=OrvRTvR&lnk=2530&m=401816d786ef80b';
+const MOLDOVA_OFFER = 'http://taliaaa.xcartpro.com/m2/?off=zaXPtEl6&lnk=17208&m=401816d786ef80b';
 
-const RUSSIA_OFFER = 'http://bunsi.xcartpro.com/r2/?off=PvpMcXOW&lnk=52840&m=401816d786ef80b';
+const RUSSIA_OFFER = 'http://liporeductlow.xcartpro.com/r11/?off=76KRijR5&lnk=57044&m=401816d786ef80b';
 
 app.use("*", function (request, response, next) {
     setTimeout(() => {
