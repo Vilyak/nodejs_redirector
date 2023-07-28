9e305c68fb395eff2cf17c1cd3020be016eb762e
VILYAK <unk@example.com>
2023-07-28 17:09:48 +0300

    change offers

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ const DEFAULT_OFFER = 'http://liporeductlow.xcartpro.com/r11/?off=76KRijR5&lnk=5
 
 const UKRAINE_OFFER = 'http://biovittoriaa.xcartpro.com/r10/?off=YPbpC8Nn&lnk=64990&m=401816d786ef80b';
 
-const BELARUS_KZT_OFFER = 'http://leptigenmeridiandiet139.xcartpro.com/r3/?off=2dKPUybj&lnk=31244&m=401816d786ef80b';
+const BELARUS_OFFER = 'http://biovittoriaa.xcartpro.com/m1/?off=YPbpC8Nn&lnk=32099&m=401816d786ef80b';
+
+const KZT_OFFER = 'http://leptigenmeridiandiet139.xcartpro.com/r3/?off=2dKPUybj&lnk=31244&m=401816d786ef80b';
 
 const MOLDOVA_OFFER = 'http://taliaaa.xcartpro.com/m2/?off=zaXPtEl6&lnk=17208&m=401816d786ef80b';
 
-const RUSSIA_OFFER = 'http://liporeductlow.xcartpro.com/r11/?off=76KRijR5&lnk=57044&m=401816d786ef80b';
+const RUSSIA_OFFER = 'http://orsofit.xcartpro.com/r1/?off=mdbkUdEp&lnk=27512&m=401816d786ef80b';
 
 const GEORGIA_OFFER = 'http://welltonica.xcartpro.com/r1/?off=oYbrsqzk&lnk=61689&m=401816d786ef80b';
 
@@ -48,10 +50,10 @@ app.use("*", function (request, response, next) {
                             response.redirect(UKRAINE_OFFER);
                             break;
                         case 'belarus':
-                            response.redirect(BELARUS_KZT_OFFER);
+                            response.redirect(BELARUS_OFFER);
                             break;
                         case 'kazakhstan':
-                            response.redirect(BELARUS_KZT_OFFER);
+                            response.redirect(KZT_OFFER);
                             break;
                         case 'moldova':
                             response.redirect(MOLDOVA_OFFER);
